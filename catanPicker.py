c531438ef744916fdcc4b628a660d0650316b0f6
vineethk96 <vineethk@vt.edu>
2020-05-22 13:16:39 -0700

    Instead of using a single array of tuples, I switched to 2 different arrays. This will allow for easier item verification.

--- catanPicker.py
+++ catanPicker.py
@@ -16,37 +16,78 @@ class HexRes(Enum):
 
 def readCSV():
 
-    hexArray = []
+    hexResArr = []
+    hexNumArr = []
 
     with open('catanBoard.csv') as csvDataFile:
         csv_reader = csv.reader(csvDataFile)
         line_count = 0
+
         for row in csv_reader:
-            hexTuple = (row[0], row[1])
-            hexArray.append(hexTuple)
+            hexResArr.append(row[0].title())
+            hexNumArr.append(row[1])
 
-        hexArray.pop(0)
+        hexResArr.pop(0)
+        hexNumArr.pop(0)
 
-    if verifyCSV(hexArray):
-        return hexArray
-    else:
-        # throw error
-        print("error has occured with the array")
-        return
+    try:
+        verifyResArr(hexResArr)
+    except:
+        print("Resources have an issue.")
+
+    try:
+        verifyNumArr(hexNumArr)
+    except:
+        print("Numbers have an issue.")
+
 
     #print(hexArray)
 
-def verifyCSV(hexArray):
+def verifyResArr(hexResArr):
+    print("hexResArr")
+    WoodCnt = 4
+    BrickCnt = 3
+    WheatCnt = 4
+    SheepCnt = 4
+    OreCnt = 3
+    DesertCnt = 1
 
-    for tuple in hexArray:
-        if( tuple[0] not in HexRes.__members__ ):
-            print(tuple[0] + " is not a memeber")
+    for item in hexResArr:
+
+        if( item not in HexRes.__members__ ):
+            print(item + " is not a memeber")
             return False
+        else:
+            if item == "Wood":
+                WoodCnt -= 1
+            elif item == "Brick":
+                BrickCnt -= 1
+            elif item == "Wheat":
+                WheatCnt -= 1
+            elif item == "Sheep":
+                SheepCnt -= 1
+            elif item == "Ore":
+                OreCnt -= 1
+            elif item == "Desert":
+                DesertCnt -= 1
 
+    if WoodCnt != 0 or BrickCnt != 0 or WheatCnt != 0 or SheepCnt != 0 or OreCnt != 0 or DesertCnt != 0:
+        print("Incorrect number of resources.")
+        return False
+
+    return True
+
+def verifyNumArr(hexNumArr):
+
+    for item in hexNumArr:
         try:
-            value = int(tuple[1])
+            value = int(item)
         except ValueError:
             print("An item other than an Int was passed")
+
+    my_dict = {i:hexNumArr.count(i) for i in hexNumArr}
+
+    print(my_dict)
 
     return True
 
