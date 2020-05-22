09ced79926c2e27b227a1f3390aaca919cb6cbf3
vineethk96 <vineethk@vt.edu>
2020-05-22 11:32:11 -0700

    CSV has been successfully imported into the program, and verified.

--- catanPicker.py
+++ catanPicker.py
@@ -3,6 +3,15 @@
 # Let's Begin Now
 
 import csv
+from enum import Enum
+
+class HexRes(Enum):
+    Wood = "Wood"
+    Brick = "Brick"
+    Wheat = "Wheat"
+    Sheep = "Sheep"
+    Ore = "Ore"
+    Desert = "Desert"
 
 def readCSV():
 
@@ -12,8 +21,32 @@ def readCSV():
         csv_reader = csv.reader(csvDataFile)
         line_count = 0
         for row in csv_reader:
-            print(row)
-            #change has been made
+            hexTuple = (row[0], row[1])
+            hexArray.append(hexTuple)
+
+        hexArray.pop(0)
+
+    if verifyCSV(hexArray):
+        return hexArray
+    else:
+        # throw error
+        print("error has occured with the array")
+
+    #print(hexArray)
+
+def verifyCSV(hexArray):
+
+    for tuple in hexArray:
+        if( tuple[0] not in HexRes.__members__ ):
+            print(tuple[0] + " is not a memeber")
+            return False
+
+        try:
+            value = int(tuple[1])
+        except ValueError:
+            print("An item other than an Int was passed")
+
+    return True
 
 
 def main():
